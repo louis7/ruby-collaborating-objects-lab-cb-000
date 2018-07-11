1541f3f42067adce30056e1db464edf2e3f811c4
louis baptiste <louisvis@ymail.com>
2018-07-11 01:29:52 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,19 @@
+require 'pry'
 class MP3Importer
-  attr_reader :path
+  attr_accessor :path
 
   def initialize(path)
     @path = path
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    files = Dir.glob("#{path}/*.mp3")
+    files.collect do|song|
+      song.gsub("./spec/fixtures/mp3s/","")
+    end
   end
 
   def import
-  files.each{ |filename| Song.new_by_filename(filename) }
-end
+    files.each{ |filename| Song.new_by_filename(filename) }
+  end
 end
